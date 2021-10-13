9144c0c633161110c1fa794a8b0e9f865fe2acec
Himanshu Singh <51034346+virtualcookie@users.noreply.github.com>
2021-10-12 23:30:52 -0700

    Add an Org publish workflow

---
+++ build-site.el
@@ -0,0 +1,64 @@
+;;; build-site.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Himanshu Singh
+;;
+;; Author: Himanshu Singh <https://github.com/himanshu>
+;; Maintainer: Himanshu Singh <to.himanshu.singh@gmail.com>
+;; Created: October 12, 2021
+;; Modified: October 12, 2021
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/himanshu/build-site
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+;; Load the publishing system
+(require 'ox-publish)
+;; Define the publishing project
+(setq org-publish-project-alist
+      (list
+       (list "jumboflap lives!"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html
+             :with-author nil
+             :with-creator t
+             :with-toc t
+             :section-numbers nil
+             :time-stamp-file nil)))
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+
+;; Generate the site output
+(org-publish-all t)
+(message "Build complete!")
+
+
+(provide 'build-site)
+;;; build-site.el ends here
